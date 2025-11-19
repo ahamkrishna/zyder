5c340c5d81e219311d91e2e1101d3e75df16cb78
K Vamshi Krishna <119439763+ahamkrishna@users.noreply.github.com>
2025-11-19 17:40:49 +0530

    Landing page

---
+++ src/components/LandingPage.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+function LandingPage() {
+  return (
+    <div className='w-full h-screen bg-zinc-900 pt-1'>
+        <div className="textStructure mt-32 px-20">
+            <div className="masker">
+               <h1 className='uppercase text-9xl leading-[8vw] traclking-tighter font-sans font-semibold'>One-Stop Solution</h1>
+            </div>
+            <div className="masker">
+               <h1 className='uppercase text-9xl leading-[8vw] traclking-tighter font-sans font-semibold'>for all your </h1>
+            </div>
+            <div className="masker">
+               <h1 className='uppercase text-9xl leading-[8vw] traclking-tighter font-sans font-semibold'>Last Mile Needs</h1>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default LandingPage
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,45 @@
+import React from 'react'
+
+
+import zyderLogo from '../assets/zyderLogo.jpg';
+
+function Navbar() {
+  return (
+    <div className='fixed z-[999] w-full px-20 py-8 font-sans flex justify-between items-center'>
+      <div className="logo w-32 h-5">
+        {/* <img src={zyderLogo} alt="zyder logo" /> */}
+        <svg width="150" height="40" viewBox="0 0 300 80" xmlns="http://www.w3.org/2000/svg">
+        <defs>
+            <linearGradient id="zyderNavGradient" x1="0%" y1="0%" x2="100%" y2="0%">
+            <stop offset="0%" stop-color="#7C3AED"/>
+            <stop offset="100%" stop-color="#0EA5E9"/>
+            </linearGradient>
+        </defs>
+
+        {/* <!-- Transparent background --> */}
+        <rect width="100%" height="100%" fill="transparent"/>
+
+        {/* <!-- Navbar Logo Text --> */}
+        <text
+            x="0"
+            y="55"
+            fill="url(#zyderNavGradient)"
+            font-size="60"
+            font-family="system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', sans-serif"
+            font-weight="600"
+            letter-spacing="1"
+        >
+            Zyder
+        </text>
+        </svg>
+
+
+      </div>
+      <div className="links flex gap-10">
+        {["Home", "Features", "Testimonials", "Contact"].map((item, index)=>(<a key={index} className='text-md capitalize font-semibold'>{item}</a>))}
+      </div>
+    </div>
+  )
+}
+
+export default Navbar
